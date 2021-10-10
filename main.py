86ea97da2530fabe2f067f49ad5206f130d54763
Kacper-Sleziak <57037642+Kacper-Sleziak@users.noreply.github.com>
2021-10-10 10:10:22 +0200

    Update

--- main.py
+++ main.py
@@ -4,30 +4,44 @@ matrix = np.matrix
 
 file_name = 'a280.tsp'
 file_path = 'test_files/' + file_name
+matrix = np.matrix([])
 
 with open(file_path, 'r') as f:
-
     for count, line in enumerate(f):
         pass
 
     f.seek(0)
     index_of_line = 0
     last_line_index = count
-    OPT = 0
+    OPT = ""
 
     for line in f:
+
+        number = ""
+        matrix_row = np.array([])
+        print(f"Line index {index_of_line}")
+
         if index_of_line == last_line_index:
-            OPT = int(line)
+            for char in line:
+                OPT += char
+            OPT = int(OPT)
+            print(OPT)
 
-        if index_of_line >= 2:
-            print(line)
+        elif index_of_line >= 2:
+            for char in line:
+                if char != " ":
+                    number += char
 
+                else:
+                    if number != "":
+                        matrix_element = int(number)     #numer do dodania do rzedu macierzy
+                        np.append(matrix_row, matrix_element)
+                        number = ""
+
+            matrix_element = int(number)
+            np.append(matrix_row, matrix_element)
+
+        np.append(matrix, matrix_row)
         index_of_line += 1
 
-
-
-
-
-
-
-
+print(matrix)
---
+++ test_files/test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+matrix = np.matrix
+array1 = np.array[3, 4, 4]
+array2 = np.array([5, 7, 8])
+
+np.append(matrix, array1)
+np.append(matrix, array2)
